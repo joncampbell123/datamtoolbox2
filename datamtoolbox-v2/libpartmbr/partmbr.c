c8b24b187662abd516a8dea786f39d439a7de25d
Jonathan Campbell <jonathan@castus.tv>
2015-11-08 19:55:35 -0800

    more

---
+++ datamtoolbox-v2/libpartmbr/partmbr.c
@@ -0,0 +1,95 @@
+#include <stdlib.h>
+#if !defined(_MSC_VER)
+# include <unistd.h>
+#endif
+#include <sys/stat.h>
+#include <stdint.h>
+#include <string.h>
+#include <assert.h>
+#include <endian.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <errno.h>
+
+#include <datamtoolbox-v2/libint13chs/int13chs.h>
+#include <datamtoolbox-v2/libpartmbr/partmbr.h>
+
+const char *libpartmbr_partition_type_to_str(const uint8_t t) {
+	switch (t) {
+		case 0x00: return "empty";
+		case 0x01: return "FAT12 (first 32MB)";
+		case 0x02: return "XENIX root";
+		case 0x03: return "XENIX usr";
+		case 0x04: return "FAT16 (first 32MB, <65536)";
+		case 0x05: return "Extended (CHS)";
+		case 0x06: return "FAT16B (CHS) (first 8GB, >=65536)";
+		case 0x07: return "IFS/HPFS/NTFS/exFAT";
+		case 0x08: return "Logical sectored FAT12/FAT16";
+
+		case 0x0B: return "FAT32 (CHS)";
+		case 0x0C: return "FAT32 (LBA)";
+
+		case 0x0E: return "FAT16B (LBA)";
+		case 0x0F: return "Extended (LBA)";
+	};
+
+	return "";
+}
+
+const char *libpartmbr_type_str[LIBPARTMBR_TYPE_MAX] = {
+	"classic",
+	"modern",
+	"AAP",
+	"NEWLDR",
+	"AST/SPEEDSTOR",
+	"Disk manager"
+};
+
+void libpartmbr_state_zero(struct libpartmbr_state_t *x) {
+	x->type = LIBPARTMBR_TYPE_CLASSIC;
+	x->entries = 4;
+}
+
+int libpartmbr_state_probe(struct libpartmbr_state_t *x,libpartmbr_sector_t sct) {
+	/* the sector must end in 0x55 0xAA */
+	if (memcmp(sct+0x1FE,"\x55\xAA",2)) return 1;
+	x->type = LIBPARTMBR_TYPE_CLASSIC;
+	x->entries = 4;
+	return 0;
+}
+
+int libpartmbr_read_entry(struct libpartmbr_entry_t *ent,const struct libpartmbr_state_t *state,const libpartmbr_sector_t sector,const unsigned int entry) {
+	if (entry >= state->entries) return 1;
+
+	/* classic MBR reading, one of 4 entries */
+	const struct libpartmbr_entry_t *s = (const struct libpartmbr_entry_t*)((const unsigned char*)sector + 0x1BE + (entry * 0x10));
+	assert(sizeof(*ent) == 0x10);
+	*ent = *s;
+
+	/* next, convert byte order for the caller */
+	ent->number_lba_sectors = le32toh(ent->number_lba_sectors);
+	ent->first_lba_sector = le32toh(ent->first_lba_sector);
+	return 0;
+}
+
+int libpartmbr_write_entry(libpartmbr_sector_t sector,const struct libpartmbr_entry_t *ent,const struct libpartmbr_state_t *state,const unsigned int entry) {
+	if (entry >= state->entries) return 1;
+
+	/* classic MBR reading, one of 4 entries */
+	struct libpartmbr_entry_t *d = (struct libpartmbr_entry_t*)((unsigned char*)sector + 0x1BE + (entry * 0x10));
+	assert(sizeof(*ent) == 0x10);
+	*d = *ent;
+
+	/* next, convert byte order for the caller */
+	d->number_lba_sectors = le32toh(d->number_lba_sectors);
+	d->first_lba_sector = le32toh(d->first_lba_sector);
+	return 0;
+}
+
+int libpartmbr_sanity_check() {
+	if (sizeof(struct libpartmbr_entry_t) != 16) return 0;
+	if (sizeof(struct int13h_packed_geometry_t) != 3) return 0;
+	return -1;
+}
+
--- datamtoolbox-v2/libpartmbr/partmbr.h
+++ datamtoolbox-v2/libpartmbr/partmbr.h
@@ -1,3 +1,64 @@
 
 #define LIBPARTMBR_SECTOR_SIZE	512
 
+enum {
+	LIBPARTMBR_TYPE_EMPTY=0x00,				// empty
+	LIBPARTMBR_TYPE_FAT12_32MB=0x01,			// FAT12 within the first 32MB, or anywhere in logical drivve
+	LIBPARTMBR_TYPE_XENIX_ROOT=0x02,			// XENIX root
+	LIBPARTMBR_TYPE_XENIX_USR=0x03,				// XENIX usr
+	LIBPARTMBR_TYPE_FAT16_32MB=0x04,			// FAT16 within the first 32MB, less than 65536 sectors, or anywhere in logical drive
+	LIBPARTMBR_TYPE_EXTENDED_CHS=0x05,			// extended partition (CHS mapping)
+	LIBPARTMBR_TYPE_FAT16B_8GB=0x06,			// FAT16B (CHS) within the first 8GB, 65536 or more sectors, or FAT12/FAT16 outside first 32MB, or in type 0x05 extended part
+	LIBPARTMBR_TYPE_NTFS_HPFS=0x07,				// OS/2 IFS/HPFS, Windows NT NTFS, Windows CE exFAT
+	LIBPARTMBR_TYPE_LOGSECT_FAT16=0x08,			// Logically sectored FAT12/FAT16 (larger sectors to overcome limits)
+
+	LIBPARTMBR_TYPE_FAT32_CHS=0x0B,				// FAT32 (CHS)
+	LIBPARTMBR_TYPE_FAT32_LBA=0x0C,				// FAT32 (LBA)
+
+	LIBPARTMBR_TYPE_FAT16B_LBA=0x0E,			// FAT16B (LBA)
+	LIBPARTMBR_TYPE_EXTENDED_LBA=0x0F,			// extended partition (LBA mapping)
+};
+
+enum libpartmbr_type_t {
+	LIBPARTMBR_TYPE_CLASSIC=0,
+	LIBPARTMBR_TYPE_MODERN,
+	LIBPARTMBR_TYPE_AAP,
+	LIBPARTMBR_TYPE_NEWLDR,
+	LIBPARTMBR_TYPE_AST_SPEEDSTOR,
+	LIBPARTMBR_TYPE_DISK_MANAGER,
+
+	LIBPARTMBR_TYPE_MAX
+};
+
+struct libpartmbr_state_t {
+	enum libpartmbr_type_t	type;
+	unsigned char		entries;
+};
+
+#pragma pack(push,1)
+struct libpartmbr_entry_t {
+	uint8_t				bootable_flag;		/* +0 or "status" or "physical flag". 0x80 for bootable, 0x00 for not */
+	struct int13h_packed_geometry_t	first_chs_sector;	/* +1 first absolute sector */
+	uint8_t				partition_type;		/* +4 */
+	struct int13h_packed_geometry_t	last_chs_sector;	/* +5 last absolute sector */
+	uint32_t			first_lba_sector;	/* +8 CONVERTED TO/FROM HOST BYTE ORDER */
+	uint32_t			number_lba_sectors;	/* +12 CONVERTED TO/FROM HOST BYTE ORDER */
+};								/* =16 */
+#pragma pack(pop)
+
+typedef uint8_t libpartmbr_sector_t[LIBPARTMBR_SECTOR_SIZE];
+
+void libpartmbr_state_zero(struct libpartmbr_state_t *x);
+int libpartmbr_state_probe(struct libpartmbr_state_t *x,libpartmbr_sector_t sct);
+int libpartmbr_read_entry(struct libpartmbr_entry_t *ent,const struct libpartmbr_state_t *state,const libpartmbr_sector_t sector,const unsigned int entry);
+int libpartmbr_write_entry(libpartmbr_sector_t sector,const struct libpartmbr_entry_t *ent,const struct libpartmbr_state_t *state,const unsigned int entry);
+const char *libpartmbr_partition_type_to_str(const uint8_t t);
+int libpartmbr_sanity_check();
+
+extern const char *libpartmbr_type_str[LIBPARTMBR_TYPE_MAX];
+
+static inline const char *libpartmbr_type_to_string(enum libpartmbr_type_t x) {
+	if (x >= LIBPARTMBR_TYPE_MAX) return "";
+	return libpartmbr_type_str[x];
+}
+
