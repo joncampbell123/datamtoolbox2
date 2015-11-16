d27cdd476d2342ddedae8ec6e006c114a7d96eae
Jonathan Campbell <jonathan@castus.tv>
2015-11-15 17:22:40 -0800

    move out defs

---
+++ datamtoolbox-v2/libmsfat/libmsfat.c
@@ -0,0 +1,39 @@
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <datamtoolbox-v2/libmsfat/libmsfat.h>
+
+int libmsfat_sanity_check() {
+	{
+		struct libmsfat_bootsector bs;
+
+		if (sizeof(bs) != 90) return -1;
+		if (sizeof(bs.BS_header) != 11) return -1;
+		if (sizeof(bs.BPB_common) != 25) return -1;
+		if (sizeof(bs.at36.BPB_FAT) != 26) return -1;
+		if (sizeof(bs.at36.BPB_FAT32) != 54) return -1;
+		if (sizeof(bs.at36) != 54) return -1;
+		if (offsetof(struct libmsfat_bootsector,BS_header) != 0) return -1;
+		if (offsetof(struct libmsfat_bootsector,BPB_common) != 11) return -1;
+		if (offsetof(struct libmsfat_bootsector,at36) != 36) return -1;
+		if (offsetof(struct libmsfat_bootsector,at36.BPB_FAT) != 36) return -1;
+		if (offsetof(struct libmsfat_bootsector,at36.BPB_FAT32) != 36) return -1;
+	}
+
+	return 0;
+}
+
+int libmsfat_bs_is_fat32(struct libmsfat_bootsector *p_bs) {
+	if (p_bs == NULL) return 0;
+
+	/* NTS: No byte swapping is needed to compare against zero.
+	 * We're checking for values that, if zero, would render FAT12/FAT16 unreadable to systems that do not understand FAT32.
+	 * Especially important is the FATSz16 field. */
+	if (p_bs->BPB_common.BPB_RootEntCnt == 0U && p_bs->BPB_common.BPB_TotSec16 == 0U && p_bs->BPB_common.BPB_FATSz16 == 0U)
+		return 1;
+
+	return 0;
+}
+
