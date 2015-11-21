6e9157fbb2ac082efea4c7a16074e454906a8ac0
Jonathan Campbell <jonathan@castus.tv>
2015-11-21 14:57:42 -0800

    more

---
+++ datamtoolbox-v2/polyfill/unix.h
@@ -0,0 +1,11 @@
+
+/* Microsoft C++ does not have S_ISREG */
+#if defined(_MSC_VER) && !defined(S_ISREG) && defined(_S_IFREG)
+# define S_ISREG(x) (x & _S_IFREG)
+#endif
+
+/* Linux does not have O_BINARY. That's primarily a DOS/Windows thing nowadays */
+#ifndef O_BINARY
+# define O_BINARY 0
+#endif
+
