3a7f31b1d1e9a0a24acec8dac371bcdcadbaeffe
Jonathan Campbell <jonathan@castus.tv>
2015-11-22 04:08:56 -0800

    readdir/rewinddir implementation

---
+++ datamtoolbox-v2/libmsfat/fio3.c
@@ -0,0 +1,506 @@
+#if defined(_MSC_VER)
+# include <datamtoolbox-v2/polyfill/ms_posix_stfu.h>
+#endif
+#if !defined(_MSC_VER)
+# include <unistd.h>
+# include <endian.h>
+#endif
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stddef.h>
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <ctype.h>
+#if defined(_MSC_VER)
+# include <datamtoolbox-v2/polyfill/ms_cpp.h>
+#endif
+#include <datamtoolbox-v2/polyfill/lseek.h>
+#include <datamtoolbox-v2/polyfill/unix.h>
+
+#include <datamtoolbox-v2/libpartmbr/mbrctx.h>
+#include <datamtoolbox-v2/libmsfat/libmsfat.h>
+
+static unsigned char			sectorbuf[512];
+
+void libmsfat_dirent_lfn_to_str_printable(char *buf,size_t buflen,const struct libmsfat_lfn_assembly_t *lfn_name) {
+	const uint16_t *s,*se;
+	char *bufend,*d;
+
+	if (buf == NULL || buflen <= (size_t)13) return;
+	bufend = buf + buflen - 1;
+	d = buf;
+
+	if (lfn_name == NULL) {
+		*d = 0;
+		return;
+	}
+	if (!lfn_name->name_avail) {
+		*d = 0;
+		return;
+	}
+
+	s = lfn_name->assembly;
+	se = s + ((5U+6U+2U)*lfn_name->name_avail);
+	assert((char*)se <= (((char*)lfn_name->assembly) + sizeof(lfn_name->assembly)));
+
+	/* TODO: conversion to UTF-8 on Linux systems */
+	while (s < se && d < bufend) {
+		if (*s == 0) break;
+
+		if (*s >= 0x80U)
+			*d++ = '?';
+		else
+			*d++ = (char)(*s);
+
+		s++;
+	}
+
+	*d = 0;
+	assert(d <= bufend);
+}
+
+void libmsfat_dirent_volume_label_to_str(char *buf,size_t buflen,const struct libmsfat_dirent_t *dirent) {
+	const char *s,*se;
+	char *bufend,*d;
+
+	if (buf == NULL || buflen <= (size_t)13) return;
+	bufend = buf + buflen - 1;
+	d = buf;
+
+	if (dirent == NULL) {
+		*d = 0;
+		return;
+	}
+
+	/* DIR_Name is immediately followed by DIR_Ext */
+	s = dirent->a.n.DIR_Name;
+	se = s + sizeof(dirent->a.n.DIR_Name) + sizeof(dirent->a.n.DIR_Ext) - 1;
+	while (se >= s && (*se == ' ' || *se == 0)) se--;
+	se++;
+	while (s < se && d < bufend) *d++ = *s++;
+
+	*d = 0;
+	assert(d <= bufend);
+}
+
+void libmsfat_dirent_filename_to_str(char *buf,size_t buflen,const struct libmsfat_dirent_t *dirent) {
+	const char *s,*se;
+	char *bufend,*d;
+
+	if (buf == NULL || buflen <= (size_t)13) return;
+	bufend = buf + buflen - 1;
+	d = buf;
+
+	if (dirent == NULL) {
+		*d = 0;
+		return;
+	}
+
+	s = dirent->a.n.DIR_Name;
+	se = s + sizeof(dirent->a.n.DIR_Name) - 1;
+	while (se >= s && (*se == ' ' || *se == 0)) se--;
+	se++;
+	while (s < se && d < bufend) *d++ = *s++;
+
+	s = dirent->a.n.DIR_Ext;
+	se = s + sizeof(dirent->a.n.DIR_Ext) - 1;
+	while (se >= s && (*se == ' ' || *se == 0)) se--;
+	se++;
+	if (s < se) {
+		*d++ = '.';
+		while (s < se && d < bufend) *d++ = *s++;
+	}
+
+	*d = 0;
+	assert(d <= bufend);
+}
+
+int libmsfat_file_io_ctx_rewinddir(struct libmsfat_file_io_ctx_t *fioctx,struct libmsfat_context_t *msfatctx,struct libmsfat_lfn_assembly_t *lfn_name) {
+	if (fioctx == NULL || msfatctx == NULL) return -1;
+	if (lfn_name != NULL) libmsfat_lfn_assembly_init(lfn_name);
+	if (libmsfat_file_io_ctx_lseek(fioctx,msfatctx,(uint32_t)0)) return -1;
+	if (libmsfat_file_io_ctx_tell(fioctx,msfatctx) != (uint32_t)0) return -1;
+	return 0;
+}
+
+int libmsfat_file_io_ctx_readdir(struct libmsfat_file_io_ctx_t *fioctx,struct libmsfat_context_t *msfatctx,struct libmsfat_lfn_assembly_t *lfn_name,struct libmsfat_dirent_t *dirent) {
+	if (fioctx == NULL || msfatctx == NULL || dirent == NULL) return -1;
+	if (lfn_name != NULL) libmsfat_lfn_assembly_init(lfn_name);
+
+	assert(sizeof(*dirent) == 32);
+	do {
+		if (libmsfat_file_io_ctx_read(fioctx,msfatctx,dirent,sizeof(*dirent)) != sizeof(*dirent))
+			return -1;
+
+		if (libmsfat_lfn_dirent_is_lfn(dirent)) {
+			if (lfn_name != NULL) {
+				if (libmsfat_lfn_dirent_assemble(lfn_name,dirent))
+					libmsfat_lfn_assembly_init(lfn_name);
+			}
+		}
+		else {
+			if (lfn_name != NULL) {
+				if (libmsfat_lfn_dirent_complete(lfn_name,dirent))
+					libmsfat_lfn_assembly_init(lfn_name);
+			}
+
+			/* 0x00, end of directory */
+			if (dirent->a.n.DIR_Name[0] == 0x00)
+				return -1;
+			/* 0xE5, deleted entry */
+			if (dirent->a.n.DIR_Name[0] == 0xE5)
+				continue;
+
+			/* found one! */
+			break;
+		}
+	} while (1);
+
+	return 0;
+}
+
+int main(int argc,char **argv) {
+	struct libmsfat_disk_locations_and_info locinfo;
+	struct libmsfat_file_io_ctx_t *fioctx = NULL;
+	struct libmsfat_context_t *msfatctx = NULL;
+	struct libmsfat_lfn_assembly_t lfn_name;
+	struct libmsfat_dirent_t dirent;
+	uint32_t first_lba=0,size_lba=0;
+	const char *s_partition = NULL;
+	const char *s_cluster = NULL;
+	libmsfat_cluster_t cluster=0;
+	const char *s_image = NULL;
+	char tmp[512];
+	int i,fd;
+
+	for (i=1;i < argc;) {
+		const char *a = argv[i++];
+
+		if (*a == '-') {
+			do { a++; } while (*a == '-');
+
+			if (!strcmp(a,"image")) {
+				s_image = argv[i++];
+			}
+			else if (!strcmp(a,"partition")) {
+				s_partition = argv[i++];
+			}
+			else if (!strcmp(a,"cluster")) {
+				s_cluster = argv[i++];
+			}
+			else {
+				fprintf(stderr,"Unknown switch '%s'\n",a);
+				return 1;
+			}
+		}
+		else {
+			fprintf(stderr,"Unexpected arg '%s'\n",a);
+			return 1;
+		}
+	}
+
+	if (libmsfat_sanity_check() != 0) {
+		fprintf(stderr,"libmsfat sanity check fail\n");
+		return 1;
+	}
+
+	if (s_image == NULL) {
+		fprintf(stderr,"fatinfo --image <image> ...\n");
+		fprintf(stderr,"\n");
+		fprintf(stderr,"--partition <n>          Hard disk image, use partition N from the MBR\n");
+		fprintf(stderr,"--cluster <n>            Which cluster to start from (if not root dir)\n");
+		return 1;
+	}
+	if (s_cluster != NULL)
+		cluster = (libmsfat_cluster_t)strtoul(s_cluster,NULL,0);
+	else
+		cluster = 0;
+
+	fd = open(s_image,O_RDONLY|O_BINARY);
+	if (fd < 0) {
+		fprintf(stderr,"Unable to open disk image, %s\n",strerror(errno));
+		return 1;
+	}
+	{
+		/* make sure it's a file */
+		struct stat st;
+		if (fstat(fd,&st) || !S_ISREG(st.st_mode)) {
+			fprintf(stderr,"Image is not a file\n");
+			return 1;
+		}
+	}
+
+	if (s_partition != NULL) {
+		struct libpartmbr_context_t *ctx = NULL;
+		int index = atoi(s_partition);
+		int dfd = -1;
+
+		if (index < 0) {
+			fprintf(stderr,"Invalid index\n");
+			return 1;
+		}
+
+		ctx = libpartmbr_context_create();
+		if (ctx == NULL) {
+			fprintf(stderr,"Cannot allocate libpartmbr context\n");
+			return 1;
+		}
+
+		/* good! hand it off to the context. use dup() because it takes ownership */
+		dfd = dup(fd);
+		if (libpartmbr_context_assign_fd(ctx,dfd)) {
+			fprintf(stderr,"libpartmbr did not accept file descriptor %d\n",fd);
+			close(dfd); // dispose of it
+			return 1;
+		}
+		dfd = -1;
+		ctx->geometry.cylinders = 16384;
+		ctx->geometry.sectors = 63;
+		ctx->geometry.heads = 255;
+
+		/* load the partition table! */
+		if (libpartmbr_context_read_partition_table(ctx)) {
+			fprintf(stderr,"Failed to read partition table\n");
+			return 1;
+		}
+
+		/* index */
+		if ((size_t)index >= ctx->list_count) {
+			fprintf(stderr,"Index too large\n");
+			return 1;
+		}
+
+		/* valid? */
+		struct libpartmbr_context_entry_t *ent = &ctx->list[(size_t)index];
+
+		if (ent->is_empty) {
+			fprintf(stderr,"You chose an empty MBR partition\n");
+			return 1;
+		}
+
+		/* show */
+		printf("Chosen partition:\n");
+		printf("    Type: 0x%02x %s\n",
+			(unsigned int)ent->entry.partition_type,
+			libpartmbr_partition_type_to_str(ent->entry.partition_type));
+
+		if (!(ent->entry.partition_type == LIBPARTMBR_TYPE_FAT12_32MB ||
+			ent->entry.partition_type == LIBPARTMBR_TYPE_FAT16_32MB ||
+			ent->entry.partition_type == LIBPARTMBR_TYPE_FAT16B_8GB ||
+			ent->entry.partition_type == LIBPARTMBR_TYPE_FAT32_CHS ||
+			ent->entry.partition_type == LIBPARTMBR_TYPE_FAT32_LBA ||
+			ent->entry.partition_type == LIBPARTMBR_TYPE_FAT16B_LBA ||
+			ent->entry.partition_type == LIBPARTMBR_TYPE_FAT12_32MB_HIDDEN ||
+			ent->entry.partition_type == LIBPARTMBR_TYPE_FAT16_32MB_HIDDEN ||
+			ent->entry.partition_type == LIBPARTMBR_TYPE_FAT16B_8GB_HIDDEN ||
+			ent->entry.partition_type == LIBPARTMBR_TYPE_FAT32_CHS_HIDDEN ||
+			ent->entry.partition_type == LIBPARTMBR_TYPE_FAT32_LBA_HIDDEN ||
+			ent->entry.partition_type == LIBPARTMBR_TYPE_FAT16B_LBA_HIDDEN)) {
+			fprintf(stderr,"MBR type does not suggest a FAT filesystem\n");
+			return 1;
+		}
+		first_lba = ent->entry.first_lba_sector;
+		size_lba = ent->entry.number_lba_sectors;
+
+		/* done */
+		ctx = libpartmbr_context_destroy(ctx);
+	}
+	else {
+		lseek_off_t x;
+
+		first_lba = 0;
+
+		x = lseek(fd,0,SEEK_END);
+		if (x < (lseek_off_t)0) x = 0;
+		x /= (lseek_off_t)512UL;
+		if (x > (lseek_off_t)0xFFFFFFFFUL) x = (lseek_off_t)0xFFFFFFFFUL;
+		size_lba = (uint32_t)x;
+		lseek(fd,0,SEEK_SET);
+	}
+
+	printf("Reading from disk sectors %lu-%lu (%lu sectors)\n",
+		(unsigned long)first_lba,
+		(unsigned long)first_lba+(unsigned long)size_lba-(unsigned long)1UL,
+		(unsigned long)size_lba);
+
+	{
+		lseek_off_t x = (lseek_off_t)first_lba * (lseek_off_t)512UL;
+
+		if (lseek(fd,x,SEEK_SET) != x || read(fd,sectorbuf,512) != 512) {
+			fprintf(stderr,"Unable to read boot sector\n");
+			return 1;
+		}
+	}
+
+	{
+		const char *err_str = NULL;
+
+		if (!libmsfat_boot_sector_is_valid(sectorbuf,&err_str)) {
+			printf("Boot sector is not valid: reason=%s\n",err_str);
+			return 1;
+		}
+	}
+
+	{
+		struct libmsfat_bootsector *p_bs = (struct libmsfat_bootsector*)sectorbuf;
+		int dfd;
+
+		if (libmsfat_bs_compute_disk_locations(&locinfo,p_bs)) {
+			printf("Unable to locate disk locations.\n");
+			return 1;
+		}
+
+		printf("Disk location and FAT info:\n");
+		printf("    FAT format:        FAT%u\n",
+			locinfo.FAT_size);
+		printf("    FAT tables:        %u\n",
+			locinfo.FAT_tables);
+		printf("    FAT table size:    %lu sectors\n",
+			(unsigned long)locinfo.FAT_table_size);
+		printf("    FAT offset:        %lu sectors\n",
+			(unsigned long)locinfo.FAT_offset);
+		printf("    Root directory:    %lu sectors\n",
+			(unsigned long)locinfo.RootDirectory_offset);
+		printf("    Root dir size:     %lu sectors\n",
+			(unsigned long)locinfo.RootDirectory_size);
+		printf("    Data offset:       %lu sectors\n",
+			(unsigned long)locinfo.Data_offset);
+		printf("    Data size:         %lu sectors\n",
+			(unsigned long)locinfo.Data_size);
+		printf("    Sectors/cluster:   %u\n",
+			(unsigned int)locinfo.Sectors_Per_Cluster);
+		printf("    Bytes per sector:  %u\n",
+			(unsigned int)locinfo.BytesPerSector);
+		printf("    Total clusters:    %lu\n",
+			(unsigned long)locinfo.Total_clusters);
+		printf("    Total data clusts: %lu\n",
+			(unsigned long)locinfo.Total_data_clusters);
+		printf("    Max clusters psbl: %lu\n",
+			(unsigned long)locinfo.Max_possible_clusters);
+		printf("    Max data clus psbl:%lu\n",
+			(unsigned long)locinfo.Max_possible_data_clusters);
+		printf("    Total sectors:     %lu sectors\n",
+			(unsigned long)locinfo.TotalSectors);
+		if (locinfo.FAT_size >= 32) {
+			printf("    FAT32 FSInfo:      %lu sector\n",
+				(unsigned long)locinfo.fat32.BPB_FSInfo);
+			printf("    Root dir cluster:  %lu\n",
+				(unsigned long)locinfo.fat32.RootDirectory_cluster);
+		}
+
+		msfatctx = libmsfat_context_create();
+		if (msfatctx == NULL) {
+			fprintf(stderr,"Failed to create msfat context\n");
+			return -1;
+		}
+		dfd = dup(fd);
+		if (libmsfat_context_assign_fd(msfatctx,dfd)) {
+			fprintf(stderr,"Failed to assign file descriptor to msfat\n");
+			close(dfd);
+			return -1;
+		}
+		dfd = -1; /* takes ownership, drop it */
+
+		msfatctx->partition_byte_offset = (uint64_t)first_lba * (uint64_t)512UL;
+		if (libmsfat_context_set_fat_info(msfatctx,&locinfo)) {
+			fprintf(stderr,"msfat library rejected disk location info\n");
+			return -1;
+		}
+	}
+
+	/* FAT32 always starts from a cluster.
+	 * FAT12/FAT16 have a dedicated area for the root directory */
+	if (locinfo.FAT_size == 32) {
+		if (s_cluster == NULL)
+			cluster = locinfo.fat32.RootDirectory_cluster;
+	}
+
+	if (cluster >= locinfo.Max_possible_clusters)
+		fprintf(stderr,"WARNING: cluster %lu >= max possible clusters %lu\n",
+			(unsigned long)cluster,
+			(unsigned long)locinfo.Max_possible_clusters);
+	else if (cluster >= locinfo.Total_clusters)
+		fprintf(stderr,"WARNING: cluster %lu >= total clusters %lu\n",
+			(unsigned long)cluster,
+			(unsigned long)locinfo.Total_clusters);
+	else if ((s_cluster != NULL || cluster != (libmsfat_cluster_t)0UL) && cluster < (libmsfat_cluster_t)2UL)
+		fprintf(stderr,"WARNING: cluster %lu is known not to have corresponding data storage on disk\n",
+			(unsigned long)cluster);
+
+	if (s_cluster != NULL || cluster != (libmsfat_cluster_t)0UL)
+		printf("Reading directory starting from cluster %lu\n",
+			(unsigned long)cluster);
+	else
+		printf("Reading directory from FAT12/FAT16 root directory area\n");
+
+	/* sanity check */
+	if (locinfo.FAT_size >= 32) {
+	}
+	else if (locinfo.FAT_size < 32 && (locinfo.RootDirectory_offset == (uint32_t)0UL || locinfo.RootDirectory_size == (uint32_t)0UL)) {
+		fprintf(stderr,"ERROR: FAT12/FAT16 root directory area does not exist\n");
+		return 1;
+	}
+
+	fioctx = libmsfat_file_io_ctx_create();
+	if (fioctx == NULL) {
+		fprintf(stderr,"Cannot alloc FIO ctx\n");
+		return 1;
+	}
+
+	if (s_cluster != NULL || cluster != (libmsfat_cluster_t)0UL) {
+		if (libmsfat_file_io_ctx_assign_cluster_chain(fioctx,msfatctx,cluster)) {
+			fprintf(stderr,"Cannot assign cluster\n");
+			return 1;
+		}
+		fioctx->is_root_dir = 1;
+		fioctx->is_directory = 1;
+	}
+	else {
+		if (libmsfat_file_io_ctx_assign_root_directory(fioctx,msfatctx)) {
+			fprintf(stderr,"Cannot assign root dir\n");
+			return 1;
+		}
+	}
+
+	if (libmsfat_file_io_ctx_rewinddir(fioctx,msfatctx,&lfn_name)) {
+		fprintf(stderr,"Cannot rewinddir\n");
+		return 1;
+	}
+
+	while (libmsfat_file_io_ctx_readdir(fioctx,msfatctx,&lfn_name,&dirent) == 0) {
+		if (dirent.a.n.DIR_Attr & libmsfat_DIR_ATTR_VOLUME_ID) {
+			libmsfat_dirent_volume_label_to_str(tmp,sizeof(tmp),&dirent);
+			printf("    <VOL>  '%s'",tmp);
+		}
+		else {
+			libmsfat_dirent_filename_to_str(tmp,sizeof(tmp),&dirent);
+			if (dirent.a.n.DIR_Attr & libmsfat_DIR_ATTR_DIRECTORY)
+				printf("    <DIR>  '%s'",tmp);
+			else
+				printf("    <FIL>  '%s'",tmp);
+		}
+		printf("\n");
+
+		if (lfn_name.name_avail) {
+			libmsfat_dirent_lfn_to_str_printable(tmp,sizeof(tmp),&lfn_name);
+			printf("        Long name:          '%s'\n",tmp);
+		}
+
+		printf("        File size:          %lu bytes\n",
+			(unsigned long)le32toh(dirent.a.n.DIR_FileSize));
+		printf("        Starting cluster:   %lu\n",
+			(unsigned long)libmsfat_dirent_get_starting_cluster(msfatctx,&dirent));
+	}
+
+	fioctx = libmsfat_file_io_ctx_destroy(fioctx);
+	msfatctx = libmsfat_context_destroy(msfatctx);
+	close(fd);
+	fd = -1;
+	return 0;
+}
+
