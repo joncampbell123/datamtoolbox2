7af17a48d1036876fd4baf427163dda4d2d4762c
Jonathan Campbell <jonathan@castus.tv>
2015-11-25 01:31:59 -0800

    cleanup UTF-16 functions

--- datamtoolbox-v2/unicode/utf16.c
+++ datamtoolbox-v2/unicode/utf16.c
@@ -1,4 +1,8 @@
 
+#if !defined(_MSC_VER)
+# include <endian.h>
+#endif
+
 #include <datamtoolbox-v2/unicode/utf16.h>
 
 /* [doc] utf16le_encode
@@ -29,21 +33,19 @@ unicode_char_t utf16le_encode(char **ptr,char *fence,unicode_char_t code) {
 	if (!p) return UTFERR_NO_ROOM;
 	if (code > (unicode_char_t)0x10FFFFUL) return UTFERR_INVALID;
 	if (code > (unicode_char_t)0xFFFFUL) { /* UTF-16 surrogate pair */
-		uint32_t lo =  (code - (unicode_char_t)0x10000UL)                          & (unicode_char_t)0x3FFUL;
-		uint32_t hi = ((code - (unicode_char_t)0x10000UL) >> (unicode_char_t)10UL) & (unicode_char_t)0x3FFUL;
+		unicode_char_t rcode = code - (unicode_char_t)0x10000UL;
+		uint16_t lo = (uint16_t)(( rcode                          & (unicode_char_t)0x3FFUL) + (unicode_char_t)0xDC00UL);
+		uint16_t hi = (uint16_t)(((rcode >> (unicode_char_t)10UL) & (unicode_char_t)0x3FFUL) + (unicode_char_t)0xD800UL);
 		if ((p+2+2) > fence) return UTFERR_NO_ROOM;
-		*p++ = (char)( (hi+(unicode_char_t)0xD800UL)                       & (unicode_char_t)0xFFUL);
-		*p++ = (char)(((hi+(unicode_char_t)0xD800UL) >> (unicode_char_t)8) & (unicode_char_t)0xFFUL);
-		*p++ = (char)( (lo+(unicode_char_t)0xDC00UL)                       & (unicode_char_t)0xFFUL);
-		*p++ = (char)(((lo+(unicode_char_t)0xDC00UL) >> (unicode_char_t)8) & (unicode_char_t)0xFFUL);
+		*((uint16_t*)p) = htole16(hi); p += 2;
+		*((uint16_t*)p) = htole16(lo); p += 2;
 	}
 	else if ((code & (unicode_char_t)0xF800UL) == (unicode_char_t)0xD800UL) { /* do not allow accidental surrogate pairs (0xD800-0xDFFF) */
 		return UTFERR_INVALID;
 	}
 	else {
 		if ((p+2) > fence) return UTFERR_NO_ROOM;
-		*p++ = (char)( code                       & (unicode_char_t)0xFFUL);
-		*p++ = (char)((code >> (unicode_char_t)8) & (unicode_char_t)0xFFUL);
+		*((uint16_t*)p) = htole16((uint16_t)code); p += 2;
 	}
 
 	*ptr = p;
@@ -74,31 +76,23 @@ unicode_char_t utf16le_encode(char **ptr,char *fence,unicode_char_t code) {
 unicode_char_t utf16le_decode(const char **ptr,const char *fence) {
 	const char *p = *ptr;
 	unicode_char_t ret;
-	int b=2;
 
 	if (!p) return UTFERR_NO_ROOM;
-	if ((p+1) >= fence) return UTFERR_NO_ROOM;
+	if ((p+2) > fence) return UTFERR_NO_ROOM;
 
-	ret = (unsigned char)p[0];
-	ret |= ((unicode_char_t)((unsigned char)p[1])) << (unicode_char_t)8UL;
-	if (ret >= (unicode_char_t)0xD800UL && ret <= (unicode_char_t)0xDBFFUL)
-		b=4;
-	else if (ret >= (unicode_char_t)0xDC00UL && ret <= (unicode_char_t)0xDFFFUL)
-		{ p++; return UTFERR_INVALID; }
-
-	if ((p+b) > fence)
-		return UTFERR_NO_ROOM;
-
-	p += 2;
+	ret = (unicode_char_t)le16toh(*((uint16_t*)p)); p += 2;
 	if (ret >= (unicode_char_t)0xD800UL && ret <= (unicode_char_t)0xDBFFUL) {
-		/* decode surrogate pair */
-		unicode_char_t hi = ret & (unicode_char_t)0x3FFUL;
-		unicode_char_t lo = (unsigned char)p[0];
-		lo |= ((unicode_char_t)((unsigned char)p[1])) << (unicode_char_t)8UL;
-		p += 2;
+		uint16_t hi,lo;
+
+		if ((p+2) > fence) return UTFERR_NO_ROOM;
+		hi = (uint16_t)(ret & (unicode_char_t)0x3FFUL);
+		lo = le16toh(*((uint16_t*)p)); p += 2;
 		if (lo < (unicode_char_t)0xDC00UL || lo > (unicode_char_t)0xDFFFUL) return UTFERR_INVALID;
 		lo &= (unicode_char_t)0x3FFUL;
-		ret = ((hi << (unicode_char_t)10UL) | lo) + (unicode_char_t)0x10000UL;
+		ret = (((unicode_char_t)hi << (unicode_char_t)10UL) + (unicode_char_t)lo) + (unicode_char_t)0x10000UL;
+	}
+	else if (ret >= (unicode_char_t)0xDC00UL && ret <= (unicode_char_t)0xDFFFUL) {
+		return UTFERR_INVALID;
 	}
 
 	*ptr = p;
